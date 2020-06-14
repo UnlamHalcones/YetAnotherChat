64f6e170597708bbcad290afdb9cab5c39b8bccc
Nicolas Cailotto <cailotto.nicoas@gmail.com>
2020-06-14 01:55:40 -0300

    avances en la creacion del server. Generacion de un cliente de prueba para probar conexiones

--- src/ar/edu/unlam/servidor/ServidorChat.java
+++ src/ar/edu/unlam/servidor/ServidorChat.java
@@ -1,9 +1,8 @@
-package ar.edu.unlam.servidor.entidades;
+package ar.edu.unlam.servidor;
 
-import java.io.DataInput;
-import java.io.DataInputStream;
+import ar.edu.unlam.servidor.threads.ThreadUsuario;
+
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
@@ -12,15 +11,14 @@ import java.util.Set;
 public class ServidorChat {
     private int port;
     private Set<String> userNames = new HashSet<>();
-    private Set<UserThread> userThreads = new HashSet<>();
-    private Set<SalaChat> salas = new HashSet<>();
+    private Set<ThreadUsuario> userThreads = new HashSet<>();
 
     public ServidorChat(int port) {
         this.port = port;
     }
 
     public static void main(String[] args) {
-        ServidorChat servidorChat = new ServidorChat(9091);
+        ServidorChat servidorChat = new ServidorChat(Integer.valueOf(args[0]));
         servidorChat.execute();
     }
 
@@ -28,16 +26,57 @@ public class ServidorChat {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Chat Server is listening on port " + port);
             while (true) {
+                // Acepto conexion
                 Socket socket = serverSocket.accept();
                 System.out.println("New user connected");
 
-                /*ThreadUsuario newUser = new ThreadUsuario(socket, this);
-                userThreads.add(newUser);
-                newUser.start();*/
+                ThreadUsuario threadUsuario = new ThreadUsuario(socket, this);
+                userThreads.add(threadUsuario);
+                threadUsuario.start();
             }
         } catch (IOException ex) {
             System.out.println("Error in the server: " + ex.getMessage());
             ex.printStackTrace();
         }
+    }
+
+    /**
+     * Delivers a message from one user to others (broadcasting)
+     */
+    public synchronized void broadcast(String message, ThreadUsuario excludeUser) {
+        for (ThreadUsuario aUser : userThreads) {
+            if (!aUser.equals(excludeUser)) {
+                aUser.sendMessage(message);
+            }
+        }
+    }
+
+    /**
+     * Stores username of the newly connected client.
+     */
+    public void addUserName(String userName) {
+        userNames.add(userName);
+    }
+
+    /**
+     * When a client is disconneted, removes the associated username and ThreadUsuario
+     */
+    public void removeUser(String userName, ThreadUsuario aUser) {
+        boolean removed = userNames.remove(userName);
+        if (removed) {
+            userThreads.remove(aUser);
+            System.out.println("The user " + userName + " quitted");
+        }
+    }
+
+    public Set<String> getUserNames() {
+        return this.userNames;
+    }
+
+    /**
+     * Returns true if there are other users connected (not count the currently connected user)
+     */
+    public boolean hasUsers() {
+        return !this.userNames.isEmpty();
     }
 }
