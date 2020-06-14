6674ccaeb15a276ea012c2b06d26f2bb7ab3ee6c
Martin <martin.pfe@gmail.com>
2020-06-14 20:26:11 -0300

    Cambio IF por switch

--- src/ar/edu/unlam/servidor/threads/ThreadUsuario.java
+++ src/ar/edu/unlam/servidor/threads/ThreadUsuario.java
@@ -11,87 +11,89 @@ import java.net.Socket;
 import java.util.Objects;
 
 public class ThreadUsuario extends Thread {
-    private Socket socket;
-    private ServidorChat server;
-    private ObjectInputStream objectInputStream;
-    private ObjectOutputStream objectOutpuStream;
-    private Usuario usuario;
+	private Socket socket;
+	private ServidorChat server;
+	private ObjectInputStream objectInputStream;
+	private ObjectOutputStream objectOutpuStream;
+	private Usuario usuario;
 
-    public ThreadUsuario(ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream,
-                         ServidorChat server, Usuario usuario) {
-        this.socket = socket;
-        this.server = server;
-        this.usuario = usuario;
-        this.objectOutpuStream = objectOutputStream;
-        this.objectInputStream = objectInputStream;
-    }
+	public ThreadUsuario(ObjectInputStream objectInputStream, ObjectOutputStream objectOutputStream,
+			ServidorChat server, Usuario usuario) {
+		this.socket = socket;
+		this.server = server;
+		this.usuario = usuario;
+		this.objectOutpuStream = objectOutputStream;
+		this.objectInputStream = objectInputStream;
+	}
 
-    public void run() {
-        try {
-            Command command = (Command) objectInputStream.readObject();
+	public void run() {
+		try {
+			Command command = (Command) objectInputStream.readObject();
 
+			while (!command.getCommandType().equals(CommandType.DISCONNECT)) {
+				// Hago un broadcast del mensaje, excluyendo al usuario que lo envia
+				// TODO hacer el switch gigante
+				switch (command.getCommandType()) {
+				case MENSAJE:
+					Mensaje clientMessage = (Mensaje) command.getInfo();
+					server.broadcast(clientMessage, this);
+					break;
+				default:
+					break;
+				}
 
-            while(!command.getCommandType().equals(CommandType.DISCONNECT)) {
-                // Hago un broadcast del mensaje, excluyendo al usuario que lo envia
-                // TODO hacer el switch gigante
-                if(command.getCommandType().equals(CommandType.MENSAJE)) {
-                    Mensaje clientMessage = (Mensaje)command.getInfo();
-                    server.broadcast(clientMessage, this);
-                }
+				command = (Command) objectInputStream.readObject();
+			}
 
+			server.removeUser(usuario, this);
+			socket.close();
 
-                command = (Command) objectInputStream.readObject();
-            }
+			String clientDisconnectMessage = usuario.getUserNickname() + " has quitted.";
+			server.broadcast(clientDisconnectMessage, this);
 
-            server.removeUser(usuario, this);
-            socket.close();
+		} catch (IOException | ClassNotFoundException ex) {
+			System.out.println("Error in ThreadUsuario: " + ex.getMessage());
+			ex.printStackTrace();
+		}
+	}
 
-            String clientDisconnectMessage = usuario.getUserNickname() + " has quitted.";
-            server.broadcast(clientDisconnectMessage, this);
+	/**
+	 * Sends a message to the client.
+	 */
+	public void sendMessage(String message) {
+		try {
+			objectOutpuStream.writeObject(message);
+		} catch (IOException e) {
+			System.err.println("Error mandando informacion al servidor." + e.getMessage());
+		}
+	}
 
-        } catch (IOException | ClassNotFoundException ex) {
-            System.out.println("Error in ThreadUsuario: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-    }
+	/**
+	 * Sends a message to the client.
+	 */
+	public void sendMessage(Mensaje message) {
+		try {
+			Command<Mensaje> mensajeCommand = new Command<>(CommandType.MENSAJE, message);
+			objectOutpuStream.writeObject(mensajeCommand);
+		} catch (IOException e) {
+			System.err.println("Error mandando informacion al servidor." + e.getMessage());
+		}
+	}
 
-    /**
-     * Sends a message to the client.
-     */
-    public void sendMessage(String message) {
-        try {
-            objectOutpuStream.writeObject(message);
-        } catch (IOException e) {
-            System.err.println("Error mandando informacion al servidor." + e.getMessage());
-        }
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		ThreadUsuario that = (ThreadUsuario) o;
+		return Objects.equals(socket, that.socket) && Objects.equals(server, that.server)
+				&& Objects.equals(objectInputStream, that.objectInputStream)
+				&& Objects.equals(objectOutpuStream, that.objectOutpuStream) && Objects.equals(usuario, that.usuario);
+	}
 
-    /**
-     * Sends a message to the client.
-     */
-    public void sendMessage(Mensaje message) {
-        try {
-            Command<Mensaje> mensajeCommand = new Command<>(CommandType.MENSAJE, message);
-            objectOutpuStream.writeObject(mensajeCommand);
-        } catch (IOException e) {
-            System.err.println("Error mandando informacion al servidor." + e.getMessage());
-        }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ThreadUsuario that = (ThreadUsuario) o;
-        return Objects.equals(socket, that.socket) &&
-                Objects.equals(server, that.server) &&
-                Objects.equals(objectInputStream, that.objectInputStream) &&
-                Objects.equals(objectOutpuStream, that.objectOutpuStream) &&
-                Objects.equals(usuario, that.usuario);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(socket, server, objectInputStream, objectOutpuStream, usuario);
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(socket, server, objectInputStream, objectOutpuStream, usuario);
+	}
 }
