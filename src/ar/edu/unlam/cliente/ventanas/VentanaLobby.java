f68d26d6f3c1567af9925a7abfd7e5ca4b4ffc71
micelidomingo <micelidomingo@gmail.com>
2020-06-20 18:26:20 -0300

    mejoras en ventana lobby

--- src/ar/edu/unlam/cliente/ventanas/VentanaLobby.java
+++ src/ar/edu/unlam/cliente/ventanas/VentanaLobby.java
@@ -1,7 +1,8 @@
 package ar.edu.unlam.cliente.ventanas;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,121 +13,100 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingConstants;
 
 import ar.edu.unlam.entidades1.*;
 
 public class VentanaLobby extends JFrame {
 
-	private static final long serialVersionUID = 716619560190543188L;
-	private JPanel panel;
-	private JPanel panelLista;
-	private JScrollPane scrollPane;
-	private JLabel etiqueta;
-	private JList<JButton> lista;
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
 	private Usuario usuario;
-	public Lobby lobby;
+	private Lobby lobby;
 	private List<VentanaChat> ventanasChat;
 
 	public VentanaLobby(Usuario usuario) {
-		
 		super();
 		System.out.println("construyo");
 		this.usuario = usuario;
-		this.panel = new JPanel();
-		this.scrollPane = new JScrollPane();
-		this.etiqueta = new JLabel();
-		this.lista = new JList<JButton>();
-		this.panelLista = new JPanel();
 		this.lobby = new Lobby();
 		this.ventanasChat = new ArrayList<>();
+		this.contentPane = new JPanel(null);
 		initialize();
 	}
 
 	private void initialize() {
-		setResizable(false);
-		setTitle("Lobby");
-
-		this.panel.setLayout(null);
-		this.panel.setPreferredSize(new Dimension(300, 580));
-
-		this.etiqueta.setText("Sala de chats disponibles:");
-		this.etiqueta.setBounds(10, 10, 200, 15);
-
-//		this.lista.removeAll();
-		this.lista.setBounds(10, 30, 300, 500);
-		this.lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.lista.setLayoutOrientation(JList.VERTICAL);
-
-		mostrarSalas();
-
-		System.out.println("AGregado el boton de crear sala");
-		this.panelLista.setLayout(null);
-		this.panelLista.setPreferredSize(new Dimension(300, 580));
-		this.panelLista.setBounds(10, 30, 300, 500);
-
-		this.scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		this.scrollPane.setViewportView(this.lista);
-		this.panelLista.add(this.scrollPane, BorderLayout.CENTER);
-
-		this.panel.add(this.etiqueta);
-		this.panel.add(this.lista);
-		this.panel.add(this.panelLista);
-		this.add(this.panel);
-
+		this.setResizable(false);
+		this.setTitle("Lobby. Usuario: " + this.usuario.getUserName());
+		this.mostrarSalas();
 		this.setSize(335, 580);
 		this.setLocationRelativeTo(null);
 	}
 
 	public void mostrarSalas() {
-		int y = 10;
+		JButton btnSala;
+		JLabel etiqueta;
+		JPanel panel;
+		JScrollPane scrollPane;
 
 		System.out.println("Tengo " + this.lobby.getSalas().size() + " salas.");
-		lista.removeAll();
-		for(SalaChat salaChat : this.lobby.getSalas()) {
-			System.out.println("pasando por la sala " + salaChat.getId());
 
-			JButton btnSala = new JButton();
+		etiqueta = new JLabel("Sala de chats disponibles:");
+		etiqueta.setBounds(10, 10, 200, 15);
+
+		panel = new JPanel();
+		panel.setBorder(null);
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(new GridLayout(20, 1, 3, 3));
+
+		// Agrega botones para unirse a las salas
+		for (SalaChat salaChat : this.lobby.getSalas()) {
+			btnSala = new JButton();
+			btnSala.setPreferredSize(new Dimension(280, 30));
 			btnSala.setText("Sala <" + salaChat.getNombreSala() + ">");
 			btnSala.setActionCommand(String.valueOf(salaChat.getId()));
-			btnSala.setHorizontalAlignment(SwingConstants.CENTER);
-			btnSala.setVerticalAlignment(SwingConstants.CENTER);
-			btnSala.setHorizontalTextPosition(SwingConstants.RIGHT);
-			btnSala.setVerticalTextPosition(SwingConstants.CENTER);
-			btnSala.setBounds(10, y, 280, 30);
 			btnSala.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					unirseAUnaSala(Long.valueOf(e.getActionCommand()));
 				}
 			});
-			lista.add(btnSala);
-			y += 35;
+			panel.add(btnSala);
 		}
-		JButton btnCrearSala = new JButton();
-		btnCrearSala.setActionCommand("-1");
-		btnCrearSala.setText("Crear una sala");
-		btnCrearSala.setIcon(new ImageIcon(new ImageIcon("src/ar/edu/unlam/cliente/Imagen/agregar.png").getImage()
+
+		// Agrega boton para crear una sala
+		btnSala = new JButton();
+		btnSala.setPreferredSize(new Dimension(280, 30));
+		btnSala.setActionCommand("-1");
+		btnSala.setText("Crear una sala");
+		btnSala.setIcon(new ImageIcon(new ImageIcon("src/ar/edu/unlam/cliente/Imagen/agregar.png").getImage()
 				.getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
-		btnCrearSala.setIconTextGap(1);
-		btnCrearSala.setHorizontalAlignment(SwingConstants.CENTER);
-		btnCrearSala.setVerticalAlignment(SwingConstants.CENTER);
-		btnCrearSala.setHorizontalTextPosition(SwingConstants.RIGHT);
-		btnCrearSala.setVerticalTextPosition(SwingConstants.CENTER);
-		btnCrearSala.setBounds(10, y, 280, 30);
-		btnCrearSala.addActionListener(new ActionListener() {
+		btnSala.setIconTextGap(1);
+		btnSala.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				crearSala();
 			}
 		});
-		lista.add(btnCrearSala);
+		panel.add(btnSala);
+
+		scrollPane = new JScrollPane(panel);
+		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		if (panel.countComponents() > 15) {
+			scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		} else {
+			scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+		}
+		scrollPane.setBorder(null);
+		scrollPane.setBounds(10, 30, 300, 500);
+
+		this.contentPane.removeAll();
+		this.contentPane.setPreferredSize(new Dimension(335, 580));
+		this.contentPane.add(scrollPane);
+		this.contentPane.add(etiqueta);
+		this.setContentPane(this.contentPane);
 	}
 
 	private void crearSala() {
@@ -159,10 +139,8 @@ public class VentanaLobby extends JFrame {
 	public void actualizarMensajes(Mensaje clientMessage) {
 		Long salaOrigenId = clientMessage.getSalaOrigenId();
 		SalaChat salaById = this.lobby.getSalaById(salaOrigenId);
-		VentanaChat ventanaChat = this.ventanasChat.stream()
-				.filter(ventana -> ventana.getSalaChat().equals(salaById))
-				.findAny()
-				.orElse(null);
+		VentanaChat ventanaChat = this.ventanasChat.stream().filter(ventana -> ventana.getSalaChat().equals(salaById))
+				.findAny().orElse(null);
 		ventanaChat.actualizarMensajes(clientMessage);
 	}
 }
